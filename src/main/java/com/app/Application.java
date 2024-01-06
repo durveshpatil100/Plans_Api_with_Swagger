a4f250aa9d9df307c19b113c40bb1128071ca3ba
durvesh <durveshp@cybage.com>
2024-01-06 14:27:57 +0530

    swagger added

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -2,10 +2,10 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@EnableWebMvc
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/app/controller/PlanController.java
+++ src/main/java/com/app/controller/PlanController.java
@@ -11,14 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.entity.Plan;
 import com.app.service.PlanService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
+@RequestMapping("/")
 public class PlanController {
 
 	@Autowired
@@ -58,6 +61,7 @@ public class PlanController {
 		return new ResponseEntity<>(plan, HttpStatus.OK);
 	}
 
+	@PutMapping("/plan")
 	public ResponseEntity<String> updatePlan(@RequestBody Plan plan) {
 		boolean isUpdated = planService.updatePlan(plan);
 
